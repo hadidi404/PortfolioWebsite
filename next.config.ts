f3278417125f5a7fee7ed710db648dd40c4ac9e6
hadidi404 <raysonbarnachia@gmail.com>
2026-01-23 16:13:46 +0800

    Remove out from git tracking

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/PortfolioWebsite",
-  assetPrefix: "/PortfolioWebsite",
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  }
+  ...(process.env.NODE_ENV === "production" && {
+    output: "export",
+    basePath: "/PortfolioWebsite",
+    assetPrefix: "/PortfolioWebsite",
+    trailingSlash: true,
+    images: {
+      unoptimized: true
+    }
+  })
 };
 
 export default nextConfig;
